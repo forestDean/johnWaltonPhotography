ef9ea15f4fd41ef38d628cef0b8833ea18ef5cb2
John Walton <113769986+forestDean@users.noreply.github.com>
2023-07-02 15:27:32 +0100

    Delete clients directory

--- clients/20170612-zoya-jorden-garden/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<div id="footer">
-  <div id="foot_left"><b><?php echo CLIENT; ?></b><p><?php echo SHOOT_DATE; ?></p></div>
-  <div id="foot_right"><?php echo TEL; ?><p><?php echo EMAIL; ?></p></div>
-  <div id="legal"><?php echo TEXT_LEGAL; ?></div>
-  <div id="copyright">&copy; JOHN WALTON <?php echo COPYRIGHT_YEAR; ?></div>
-</div>
