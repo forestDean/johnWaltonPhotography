2067b618d53e596871b4090976773498ab1b377e
John Walton <113769986+forestDean@users.noreply.github.com>
2023-07-08 10:36:30 +0100

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -46,7 +46,7 @@ $(document).ready(function() {
 <div class="container">
   <div id="main">
     <div id="galleryHeader">
-    <?php require_once ('includes/social.php')?>
+    <?php require_once (__DIR__ . '/../assets/includes/social.php')?>
     
     <div id="home"><a href=index.php><img src="../graphics/jw_button_grey30.png" width="25" height="25"></a></div>      
     </div><!--end of #galleryHeader-->
@@ -54,11 +54,11 @@ $(document).ready(function() {
     <div id="galleryHome">
     	<div id="squareOverlay"></div>
     	<div id="slideshowHome" class="slideShowHome">
-		<?php require_once ('images/squareSlides.php')?>
+	<?php require_once (__DIR__ . '/../assets/images/squareSlides.php')?>
         </div><!--end of #slideshowHome-->
     </div><!--end of #galleryHome-->
     
-	<?php require_once ('includes/contactForm.php')?>
+	<?php require_once (__DIR__ . '/../assets/includes/contactForm.php')?>
    
     <div id="controls">
     <div id="prev"><a href=#><img src="../graphics/prevNextButton30.png" width="35" height="35"></a></div>
@@ -68,7 +68,7 @@ $(document).ready(function() {
   </div><!--end of #main-->
 </div><!--end of #container-->
 
-	<?php require_once ('includes/legal.php')?>
+	<?php require_once (__DIR__ . '/../assets/includes/legal.php')?>
 
 </body>
 </html>
