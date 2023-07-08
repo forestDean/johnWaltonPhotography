a7fbcfc4aa4680918bd6bc8aa664331c6d6c7c62
John Walton <113769986+forestDean@users.noreply.github.com>
2023-07-08 11:48:44 +0100

    Update build.php

--- build.php
+++ build.php
@@ -1,16 +1,17 @@
 <?php
 
-// Set the output directory where the static HTML files will be saved
-$outputDirectory = __DIR__ . '/static/';
-
 // Define the pages you want to render as HTML
 $pages = [
     'index.php',
-    './pages/about.php',
-    './pages/prints.php',
-    './pages/contact.php',
+    'pages/about.php',
+    'pages/prints.php',
+    'pages/contact.php',
     // Add more page filenames as needed
 ];
+    
+// Set the output directory where the static HTML files will be saved
+$outputDirectory = __DIR__ . '/static/'; // Specify the output directory path
+$outputFile = $outputDirectory . $pages; // Specify the output file path
 
 // Loop through each page and render it as HTML
 foreach ($pages as $page) {
@@ -27,8 +28,12 @@ foreach ($pages as $page) {
         mkdir($outputDirectory, 0755, true);
     }
 
+    // // Replace file links in the HTML content
+    // $htmlContent = str_replace('../assets/', '/assets/', $htmlContent);
+
     // Save the rendered HTML to the output file
     file_put_contents($outputDirectory . '/' . $outputFilename, $html);
+    // file_put_contents($outputFile, $htmlContent);
 }
 
 echo 'PHP to HTML build completed successfully.';
