495b48010dde6c8319d681ad06c9a1c0111bcab3
John Walton <113769986+forestDean@users.noreply.github.com>
2023-07-08 14:27:35 +0100

    Update build.php

--- build.php
+++ build.php
@@ -33,7 +33,7 @@ foreach ($pages as $page) {
 
     // Set the output filename by replacing the .php extension with .html
     $outputFilename = str_replace('.php', '.html', $page);
-    echo $outputFilename;
+    echo ' ~ ' . $outputFilename;
     
     // // Replace file links in the HTML content
     // $htmlContent = str_replace('../assets/', '/assets/', $htmlContent);
