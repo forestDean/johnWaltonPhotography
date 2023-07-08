777a37bfa458ae096d724b377fde52d0aa656597
John Walton <113769986+forestDean@users.noreply.github.com>
2023-07-08 10:09:17 +0100

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -10,7 +10,7 @@
 <script type="text/javascript" src="jquery/jquery.cycle2.min.js"></script>
 <script type="text/javascript" src="jquery/jquery.cycle2.swipe.min.js"></script>
 
-<?php require_once ('./assets/includes/slideshowAbout.html')?>
+<?php require_once ('__DIR__ . '/../assets/includes/slideshowAbout.html')?>
 
 </head>
 
@@ -27,11 +27,11 @@
     <div id="galleryHome">
     	<div id="squareOverlay"></div>
     	<div id="slideshowHome" class="slideShowHome">
-		<?php require_once ('../assets/images/aboutSlides.php')?>
+		<?php require_once ('__DIR__ . '/../assets/images/aboutSlides.php')?>
         </div><!--end of #slideshowHome-->
     </div><!--end of #galleryHome-->
     
-    <?php require_once ('../assets/includes/aboutText.html')?>
+    <?php require_once ('__DIR__ . '/../assets/includes/aboutText.html')?>
    
     <div id="controls">
         <div id="prev"><a href=index.php><img src="../graphics/prevNextButton30.png" width="35" height="35"></a></div>
@@ -41,7 +41,7 @@
   </div><!--end of #main-->
 </div><!--end of #container-->
 
-	<?php require_once ('../assets/includes/legal.php')?>
+	<?php require_once ('__DIR__ . '/../assets/includes/legal.php')?>
 
 </body>
 </html>
