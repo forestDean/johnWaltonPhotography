683d715734aba1ec79c8010022501d31483a402b
forestDean <113769986+forestDean@users.noreply.github.com>
2023-07-08 13:16:48 +0100

    edit build.php

--- build.php
+++ build.php
@@ -1,31 +1,33 @@
 <?php
 
 // Set the output directory where the static HTML files will be saved
-$outputDirectory = __DIR__ . '/static/';
+$outputDirectory = '/static/';
+echo $outputDirectory;
+
+// Create the output directory if it doesn't exist
+if (!is_dir($outputDirectory)) {
+    mkdir($outputDirectory, 0755, true);
+}
 
 // Define the pages you want to render as HTML
 $pages = [
     'index.php',
-    './pages/about.php',
-    './pages/prints.php',
-    './pages/contact.php',
+    'pages/about.php',
+    'pages/prints.php',
+    'pages/contact.php',
     // Add more page filenames as needed
 ];
 
 // Loop through each page and render it as HTML
 foreach ($pages as $page) {
-    // Set the output filename by replacing the .php extension with .html
-    $outputFilename = str_replace('.php', '.html', $page);
 
     // Capture the rendered HTML output
     ob_start();
     include $page;
     $html = ob_get_clean();
 
-    // Create the output directory if it doesn't exist
-    if (!is_dir($outputDirectory)) {
-        mkdir($outputDirectory, 0755, true);
-    }
+    // Set the output filename by replacing the .php extension with .html
+    $outputFilename = str_replace('.php', '.html', $page);
 
     // Save the rendered HTML to the output file
     file_put_contents($outputDirectory . '/' . $outputFilename, $html);
