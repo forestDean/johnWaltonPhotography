49807e7d9957f5a0328c113625a7f0a7a986e0bd
John Walton <113769986+forestDean@users.noreply.github.com>
2023-07-09 12:25:38 +0100

    Delete current directory

--- current/images/elsewhere/autoloader/header.php
+++
@@ -1,4 +0,0 @@
-<div id="header">
-  <div id="head_left"><b>John Walton Autoloader</b> : <a href="index.php">Selection</a> : <a href="preview.php">Preview </a> : <a href="thumbnail.php">Thumbnail</a> : <a href="holding.php">Holding</a></div>
-  <div id="head_right"><?php echo date('D, d M Y H:i:s')?></div>
-</div>
