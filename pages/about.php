a7462058554d38aa22fab48811e59cc8ac506561
forestDean <113769986+forestDean@users.noreply.github.com>
2023-07-09 15:29:10 +0100

    about.html

--- pages/about.php
+++ pages/about.php
@@ -1,47 +1,95 @@
+
 <!DOCTYPE HTML>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>John Walton | About</title>
-<link rel="shortcut icon" href="https://johnwalton.photography/graphics/favicon.ico">
-<link href="includes/johnwalton.css" rel="stylesheet" type="text/css">
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>John Walton | About</title>
+  <script src="/cdn-cgi/apps/head/foOtnevuAVkIfXaorWoELjNWKOk.js"></script><link rel="shortcut icon" href="../graphics/favicon.ico">
+  <link href="../assets/includes/johnwalton.css" rel="stylesheet" type="text/css">
 
-<script type="text/javascript" src="jquery/jquery.min.js"></script>
-<script type="text/javascript" src="jquery/jquery.cycle2.min.js"></script>
-<script type="text/javascript" src="jquery/jquery.cycle2.swipe.min.js"></script>
+  <script type="e05dbd2693aa555e092083a1-text/javascript" src="jquery/jquery.min.js"></script>
+  <script type="e05dbd2693aa555e092083a1-text/javascript" src="jquery/jquery.cycle2.min.js"></script>
+  <script type="e05dbd2693aa555e092083a1-text/javascript" src="jquery/jquery.cycle2.swipe.min.js"></script>
 
-<?php require_once (__DIR__ . '/../assets/includes/slideshowAbout.html')?>
+  <script type="e05dbd2693aa555e092083a1-text/javascript">
+  $("html").addClass("js");
+  $(document).ready(function() {
+    $('#slideshowHome').cycle({
+    fx: 'fade', //scrollHorz, fade, fadeout, carousel
+    sync:    true,  // true if in/out transitions should occur simultaneously
+    easing:  'linear' , // easing method for both in and out transitions
+    slides:	'> div',
+  //	loader: true, // load slides as images arrive
+  //	progressive: "#imageLoad", // load slides as images arrive
+    paused: true, //If true the slideshow will begin in a paused state
+  //	manualSpeed: 1000,
+  //	delay:    2000,
+  //	speed: 1000,
+  //	timeout: 4000,
+  //	pauseOnHover: true,	
+    hideNonActive: true,
+    allowWrap: true,
+  //	pagerEventBubble: false,
+  //	prev:'#prev',
+  //  next:'#next'
+    });
+  }); //end of $(document).ready	
+  </script>
+  </head>
 
-</head>
-
-<body>
-
-<div class="container">
-  <div id="main">
-    <div id="galleryHeader">
-    <?php require_once (__DIR__ . '/../assets/includes/social.php')?>
-    
-    <div id="home"><a href=index.php><img src="../graphics/jw_button_grey30.png" width="25" height="25"></a></div>      
-    </div><!--end of #galleryHeader-->
-    
-    <div id="galleryHome">
-    	<div id="squareOverlay"></div>
-    	<div id="slideshowHome" class="slideShowHome">
-		<?php require_once (__DIR__ . '/../assets/images/aboutSlides.php')?>
+  <body>
+  <div class="container">
+    <div id="main">
+      <header id="galleryHeader">
+        <div id="social">
+          <a class="socialIcon" href="#"><i class="fa-brands fa-linkedin fa-xl" style="color: #b8b8b8;"></i></a>
+          <a class="socialIcon" href="#"><i class="fa-brands fa-github fa-xl" style="color: #b8b8b8;"></i></a>
+          <a class="socialIcon" href="#"><i class="fa-brands fa-facebook fa-xl" style="color: #b8b8b8;"></i></a>
+          <a class="socialIcon" href="#"><i class="fa-regular fa-envelope fa-xl" style="color: #b8b8b8;"></i></a>
+        </div><!--end of #social-->    
+      <div id="home"><a href=index.php><img src="../graphics/jw_button_grey30.png" width="25" height="25"></a></div>      
+      </header><!--end of #galleryHeader-->
+      
+      <div id="galleryHome">
+        <div id="squareOverlay"></div>
+        <div id="slideshowHome" class="slideShowHome">
+          <div class="slideDivHome" data-cycle-hash="201303041004">
+            <img src="images/about/previews/john_walton_201303041004.jpg" width="450" height="450" alt="" id="slideImage1" title="201303041004" class="slideImage">
+          </div>
+          <script id="imageLoad" type="text/cycle">
+            <div class="slideDivHome" data-cycle-hash="201303041004_1"><img src="images/about/previews/john_walton_201303041004_1.jpg" width="450" height="450" alt="" id="slideImage2" title="201303041004_1" class="slideImage"></div><br>
+          </script>
         </div><!--end of #slideshowHome-->
-    </div><!--end of #galleryHome-->
+      </div><!--end of #galleryHome-->
+      
+      
+    <div id="containerText">
+      <div id="aboutText">
+          <p>This website is a showcase of John Walton's personal work, giving the viewer both a place to enjoy his perspective &amp; the opportunity to purchase prints.</p>
+          <p>The image collections reveal the beauty of the urban margins of east London where the city &amp; country collide. Whether social or landscape subject, the dynamic conflict between conservation &amp; construction forms fleeting moments of wonder in a fragile world.</p>
+          <p>We hope you enjoy the pictures.</p>
+          </div>
+      </div>
     
-    <?php require_once (__DIR__ . '/../assets/includes/aboutText.html')?>
-   
-    <div id="controls">
-        <div id="prev"><a href=index.php><img src="../graphics/prevNextButton30.png" width="35" height="35"></a></div>
-        <div id="next"><a href=index.php><img src="../graphics/prevNextButton30.png" width="35" height="35"></a></div>
+      <footer id="controls">
+          <div id="prev"><a href=index.php><img src="../graphics/prevNextButton30.png" width="35" height="35"></a></div>
+          <div id="next"><a href=index.php><img src="../graphics/prevNextButton30.png" width="35" height="35"></a></div>
+      </footer>
+
+    </div><!--end of #main-->
+
+  </div><!--end of #container-->
+
+  <div id="print">
+    <div id="sale">
+      <p>Prints of images on this website are available to order - see prints page. Licencing for use on websites, for editorial &amp; advertising is available on request. All enquiries are welcome.</p>
     </div>
+    
+    <div id="credit"><p style="font-weight:bold">&copy;johnwalton.photography</p>
+      <p>This website &amp; its content are owned by John Walton, trading as John Walton Photography. The website, its design &amp; its content including each &amp; every photograph are protected by copyright &amp; trademark law &amp; other related property rights. Copyright in the photographs is owned by John Walton. The photographs are also protected by moral rights &amp; John Walton asserts his moral right to be identified as the author wherever &amp; whenever his photographs are copied or distributed by any means.</p>
+    </div>
+  </div>
 
-  </div><!--end of #main-->
-</div><!--end of #container-->
-
-	<?php require_once (__DIR__ . '/../assets/includes/legal.php')?>
-
-</body>
+  <script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="e05dbd2693aa555e092083a1-|49" defer></script>
+  </body>
 </html>
